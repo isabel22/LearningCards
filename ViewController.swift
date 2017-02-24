fc5b8e8a54305198d0ae45e0aa5d4cf83427d78d
Isabel <isabel@Isabels-MacBook-Pro.local>
2017-02-24 13:19:01 -0600

    Learning cards This project was created to help people to learn new languages with cards. In this case we will start with english, spanish and german.

---
+++ ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  LearningCards
+//
+//  Created by Isabel on 2/21/17.
+//  Copyright © 2017 MoyaLab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var spanishBtn: UIBarButtonItem!
+    
+    @IBOutlet weak var englishBtn: UIBarButtonItem!
+    
+    @IBOutlet weak var germanBtn: UIBarButtonItem!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }
+
+    @IBAction func translateSpanish(_ sender: Any) {
+    }
+    
+    @IBAction func translateEnglish(_ sender: Any) {
+    }
+    
+    @IBAction func translateGerman(_ sender: Any) {
+    }
+    
+
+}
+
